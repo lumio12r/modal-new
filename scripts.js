f54890dd57c2f1be3dc00e345ab851fc981f0570
lumio12r <64443320+lumio12r@users.noreply.github.com>
2021-08-21 08:14:41 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -268,8 +268,11 @@ function changeColor (Array, e) {
             parent.push(item);
         }
         for (let i = 0; i < parent.length; i++) {
+            console.log('hi');
             for (let product of object.items[parent[i]].products) {
+                console.log('hey');
                 if (options.children[i] == e.target ) {
+                    console.log('may');
                     let difference = parseFloat(product.price_difference);
                     current.classList.remove("current-set");
                     current_set = color_version[i];
@@ -285,7 +288,7 @@ function changeColor (Array, e) {
             }
         }
     }
-    }}
+    }};
 
 
 counter_input.addEventListener('input', function (e) {
